92b8101e0dce2cb2c00660e108189f0ce4e32f96
sanjuktamahato <sanjuktamahato111@gmail.com>
2025-05-23 21:29:44 +0530

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,12 @@
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+
+print(f"Addition: {num1 + num2}")
+print(f"Subtraction: {num1 - num2}")
+print(f"Multiplication: {num1 * num2}")
+print(f"Division: {num1 / num2}")
+print(f"Modulus: {num1 % num2}")
+print(f"Exponentiation: {num1 ** num2}")
+print(f"Floor Division: {num1 // num2}")
